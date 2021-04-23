0115300777c9d674509d541534ca58ba13903274
giselearaujos <giselearaujo07.s@gmail.com>
2021-04-23 13:39:56 -0300

    💡 Modified screen home

--- views/home/Css.js
+++ views/home/Css.js
@@ -2,25 +2,31 @@ import { StyleSheet } from 'react-native';
 
 const css = StyleSheet.create({
     container: {
-      flex: 1,
-      backgroundColor: '#AA2B1D',
-      alignItems: 'center',
-      justifyContent: 'center',
-    },
-    home: {      
-      width:400,       
-      padding:15,
-      marginBottom: 460,
-      borderRadius: 5,    
-    },    
-    input_home: {
-      backgroundColor: '#fff',
-      fontSize: 20,
-      padding:10,
+      flex: 1,      
+      backgroundColor: '#AA2B1D',          
+      justifyContent: 'space-evenly',   
+      alignItems: 'center',     
+    },  
+    title: {
+      color: '#000',
+      fontWeight: 'bold',
+      fontSize: 18,
+      backgroundColor: '#FFF',
+      textAlign: 'center',
       marginHorizontal: 15,
+      padding:15,
       borderRadius: 5
-    }   
-    
-  });
+    },
+    wrap: {
+      backgroundColor: '#fff',
+      height: 60,
+      width: 60,
+      marginHorizontal: 15,
+      paddingHorizontal: 150,
+      borderRadius:5,
+      alignItems: 'center',
+      justifyContent: 'center',    
+    }    
+});
 
 export {css};  
--- views/home/Home.js
+++ views/home/Home.js
@@ -1,16 +1,41 @@
 import React from 'react';
-import { View, TextInput, } from 'react-native';
+import { View, Image, TouchableOpacity, Text} from 'react-native';
 import {css} from './Css';
 
 export default function Home(){
-    return(
-        <View style={css.container}>
-           <View style={css.home}>                
-                <TextInput style={css.input_home} 
-                            placeholder='Busque sua comida favorita...'
-                    />                    
-            </View> 
-           
+    return(        
+        <View style={css.container}> 
+            <Text style={css.title}>Encontre sua comida favorita 🍱 </Text>                                      
+            <TouchableOpacity>
+                <View style={css.wrap}>
+                <Image source={require('../../assets/img/burger.png')}
+                style={css.options_foods} />                
+                </View>
+            </TouchableOpacity>
+            <TouchableOpacity>
+                <View style={css.wrap}>
+                <Image source={require('../../assets/img/pizza.png')}
+                style={css.options_foods} /> 
+                </View>
+            </TouchableOpacity>
+            <TouchableOpacity>
+                <View style={css.wrap}>
+                <Image source={require('../../assets/img/chicken.png')}
+                style={css.options_foods} /> 
+                </View>
+            </TouchableOpacity>
+            <TouchableOpacity>
+                <View style={css.wrap}>
+                <Image source={require('../../assets/img/taco.png')}
+                style={css.options_foods} /> 
+                </View>
+            </TouchableOpacity>
+            <TouchableOpacity>
+                <View style={css.wrap}>
+                <Image source={require('../../assets/img/soda.png')}
+                style={css.options_foods} /> 
+                </View>
+            </TouchableOpacity>                     
         </View>
     );
 }
